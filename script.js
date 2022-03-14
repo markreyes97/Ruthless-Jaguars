a52930560c85bf9cc19f7c4a07a8f94cafa74e56
Vinny D <vdurham@prideseals.com>
2022-03-14 11:23:36 -0400

    Basic javascript set up to call in APIs with responses

--- script.js
+++ script.js
@@ -1,22 +1,46 @@
+var cityEl = document.querySelector("#city")
 var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q="
 var apiKey = "&appid=a35536613023136e4915b74f3f80575a"
 function findWeather() {
-    var city = inputEl.value;
+    var city = cityEl.value;
+    console.log(city)
     var url = queryUrl + city + apiKey;
     fetch(url)
         .then(function(response) {
             if (response.ok)
                 response.json().then(function(data) {
-                    getWeather(data)
+                    console.log(data)
                 })
         })
 }
-const URL = 'https://aztro.sameerkumar.website/?sign=scorpio&day=today';
+
+
+const URL = 'https://aztro.sameerkumar.website/?sign=Scorpio&day=today';
 fetch(URL, {
     method: 'POST'
 })
     .then(response => response.json())
-    .then(json => {
-        const date = json.description;
-        console.log(json);
+    .then(data => {
+        const date = data.description;
+        console.log(data);
     });
+
+
+
+var selectEl = document.querySelector('#selection');
+var buttonEl = document.querySelector('#btn')
+
+buttonEl.addEventListener('click', function () {
+    findWeather()
+    var sign = selectEl.value;
+    const URL = 'https://aztro.sameerkumar.website/?sign=' + sign + '&day=today';
+    fetch(URL, {
+        method: 'POST'
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        });
+    });
+
+                    
